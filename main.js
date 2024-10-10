579dee5485a7e6dafaa1388cd04639568d95e541
Shiro <shiro.littlewhite@gmail.com>
2024-10-10 13:04:40 +0800

    10/10練習

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import './assets/scss/all.scss';
 
 console.log("Hello world!");
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = gsap.utils.toArray(".gScroll");
+
+gsap.to(sections, {
+scrollTrigger: {
+    trigger: '.content',
+    markers: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    pin: true,
+  }
+});
